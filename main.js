f3c812bd06742427a220cb837a27483caa98398d
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-08-08 11:22:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let submitAnswer = document.querySelector('.btn');
 let question = document.querySelector('#question');
 let answer = document.querySelector('#answer');
 let scoreBoard = document.querySelector('#score');
-let q101 = document.querySelector("#cat-1-100")
+let squares = document.querySelectorAll('.row-cols-5 .col');
 
 
 // Question groups
@@ -72,46 +72,57 @@ let randQ = (totalQuestions) => {
 
 gameBoard.addEventListener('click', function (event){
 
+    
     let q100 = 0;
     q100 = randQ(group100.length);
     if (event.target.innerText === '$100'){
         question.innerText = group100[q100].question;
-        score = 100;
         currentQuestion = group100[q100];
+        score = 100;
     }
 
     let q200 = 0;
     q200 = randQ(group200.length);
     if (event.target.innerText === '$200'){
         question.innerText = group200[q200].question;
-        score = 200;
         currentQuestion = group200[q200];
+        score = 200;
     }
 
     let q400 = 0;
     q400 = randQ(group400.length);
     if (event.target.innerText === '$400'){
         question.innerText = group400[q400].question;
-        score = 400;
         currentQuestion = group400[q400];
+        score = 400;
     }
 
     let q600 = 0;
     q600 = randQ(group600.length);
     if (event.target.innerText === '$600'){
         question.innerText = group600[q600].question;
-        score = 600;
         currentQuestion = group600[q600];
+        score = 600;
     }
 
     let q800 = 0;
     q800 = randQ(group800.length);
     if (event.target.innerText === '$800'){
         question.innerText = group800[q800].question;
-        score = 800;
         currentQuestion = group800[q800];
+        score = 800;
     }
 })
+
+for (let i = 0; i < squares.length; i++){
+    console.log(squares[i]);
+
+    squares[i].addEventListener('click', function(){
+        if (!squares[i].disabled){
+            squares[i].disabled;
+        }
+    })
+}
 
 
 // Button setup and answer space.
@@ -139,6 +150,18 @@ submitAnswer.addEventListener('click', function(){
         question.innerText = 'The Question!';
         alert(`Incorrect:
 The correct answer is: ${currentQuestion.answer}.`)
+        if (score === 100){
+            totalScore -= score;
+        } else if (score == 200){
+            totalScore -= score;
+        } else if (score === 400){
+            totalScore -= score;
+        } else if (score === 600){
+            totalScore -= score;
+        } else if (score === 800){
+            totalScore -= score
+        }
+        scoreBoard.innerText = `Your Score: $${totalScore}`; 
 
     }
 })
